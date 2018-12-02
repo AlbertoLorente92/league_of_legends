4d5bc9e930cc4571879de590fcef5b061bd56d88
AlbertoLorente92 <alberto.lorente92@gmail.com>
2018-12-02 21:00:19 +0100

    I like this way better for the main

--- main.py
+++ main.py
@@ -59,87 +59,16 @@ def add_complete_games(list_of_complete_games_f, json_f):
     list_of_complete_games_f.append(CompleteGame(json_f))
 
 
-def ask_the_riot_api(filename_account, filename_match_list, filename_match_info):
+def main(load_from_files, filename_account, filename_match_list, filename_match_info):
     print("PROGRAM STARTS.")
-    print("Loading configuration...")
-    config = Configuration("configuration.json")
-    print("tConfiguration loaded.")
+    if not load_from_files:
+        print("Loading configuration...")
+        config = Configuration("configuration.json")
+        print("tConfiguration loaded.")
+        method_param = {"summonerName": config.summonerName}
+
     print()
-    print("Loading champiosn and stats...")
-    champions = DictOfChampsAndStats("champion.json")
-    print("Champions and stats loaded.")
-    print()
-    list_of_games = []
-    list_of_complete_games = []
-
-    method_param = {"summonerName": config.summonerName}
-
-    print("Loading account info...")
-    json_response = json_info(config, "Info_Account", {}, method_param)
-    if json_response == "":
-        sys.exit("===Error loading account info. Program stops.===")
-    print("Account info loaded.")
-    print()
-
-    save_this_json(filename_account + ".json", json_response)
-
-    if "accountId" not in json_response.keys():
-        sys.exit("===Error. 'accountId' isn't specified.===")
-
-    method_param["accountId"] = json_response["accountId"]
-
-    print("AccountId:", method_param["accountId"])
-    print()
-
-    print("Loading match history...")
-    # I want all data from all my rank games in soloQ (queue=420) from season 11 (season=11)
-    params = {"queue": 420, "season": 11}
-    began = 0
-    end = 100
-    stop = False
-    while not stop:
-        params["beginIndex"] = began  # Ask for blocks of 100 games
-        params["endIndex"] = end
-        json_response = json_info(config, "MatchList_Account", params, method_param)
-        if json_response == "":
-            stop = True
-        else:
-            add_games(list_of_games, json_response["matches"])
-            save_this_json(filename_match_list + "_" + str(began) + "_" + str(end) + ".json", json_response)
-            began += 100
-            end += 100
-            if len(json_response["matches"]) < 100:  # If the block doesn't have 100 games it's the last block
-                stop = True
-    print("Match history loaded.")
-    print()
-    # print_list_of_games(list_of_games, champions)
-    print("Loading every game....")
-    x = 0
-    while x in range(0, len(list_of_games)):
-        method_param["matchId"] = list_of_games[x].gameId
-        json_response = json_info(config, "Info_Match", {}, method_param)
-        if json_response == "":
-            # Errors here aren't suppose to happen
-            sys.exit("===Error. Riot API shows an error.===")
-        else:
-            # pprint.pprint(json)
-            if "gameId" in json_response.keys():
-                print(x + 1, "/", len(list_of_games), "-", json_response["gameId"])
-                save_this_json(filename_match_info + "_" + str(json_response["gameId"]) + ".json", json_response)
-                add_complete_games(list_of_complete_games, json_response)
-                x += 1
-    print("Every game loaded.")
-    print()
-    show_me_everything(list_of_complete_games, champions)
-
-
-def load_from_jsons(filename_account, filename_match_list, filename_match_info):
-    print("PROGRAM STARTS.")
-    print("Loading configuration...")
-    config = Configuration("configuration.json")
-    print("tConfiguration loaded.")
-    print()
-    print("Loading champiosn and stats...")
+    print("Loading champions and stats...")
     champions = DictOfChampsAndStats("champion.json")
     print("Champions and stats loaded.")
     print()
@@ -147,13 +76,22 @@ def load_from_jsons(filename_account, filename_match_list, filename_match_info):
     list_of_complete_games = []
     print("Loading account info...")
 
-    json_response = load_this_json(filename_account + ".json")
+    if load_from_files:
+        json_response = load_this_json(filename_account + ".json")
+    else:
+        json_response = json_info(config, "Info_Account", {}, method_param)
+
     if json_response == "":
         sys.exit("===Error loading account info. Program stops.===")
     print("Account info loaded.")
     print()
     if "accountId" not in json_response.keys():
         sys.exit("===Error. 'accountId' isn't specified.===")
+
+    if not load_from_files:
+        save_this_json(filename_account + ".json", json_response)
+        method_param["accountId"] = json_response["accountId"]
+        params = {"queue": 420, "season": 11}
 
     print("AccountId:", json_response["accountId"])
     print()
@@ -164,7 +102,12 @@ def load_from_jsons(filename_account, filename_match_list, filename_match_info):
     end = 100
     stop = False
     while not stop:
-        json_response = load_this_json(filename_match_list + "_" + str(began) + "_" + str(end) + ".json")
+        if load_from_files:
+            json_response = load_this_json(filename_match_list + "_" + str(began) + "_" + str(end) + ".json")
+        else:
+            params["beginIndex"] = began  # Ask for blocks of 100 games
+            params["endIndex"] = end
+            json_response = json_info(config, "MatchList_Account", params, method_param)
         if json_response == "":
             stop = True
         else:
@@ -179,15 +122,22 @@ def load_from_jsons(filename_account, filename_match_list, filename_match_info):
     print("Loading every game....")
     x = 0
     while x in range(0, len(list_of_games)):
-        json_response = load_this_json(filename_match_info + "_" + str(list_of_games[x].gameId) + ".json")
-        if json_response == "":
+        if load_from_files:
+            json_response = load_this_json(filename_match_info + "_" + str(list_of_games[x].gameId) + ".json")
+        else:
+            method_param["matchId"] = list_of_games[x].gameId
+            json_response = json_info(config, "Info_Match", {}, method_param)
+        if json_response == "" and not load_from_files:
             # Errors here aren't suppose to happen
             sys.exit("===Error. The file '" + filename_match_info + "_" + str(json_response["gameId"]) + ".json' doesn't exits.===")
+        elif json_response == "" and load_from_files:
+            sys.exit("===Error. Riot API shows an error.===")
         else:
             # pprint.pprint(json)
             if "gameId" in json_response.keys():
                 print(x + 1, "/", len(list_of_games), "-", json_response["gameId"])
-                save_this_json(filename_match_info + "_" + str(json_response["gameId"]) + ".json", json_response)
+                if not load_from_files:
+                    save_this_json(filename_match_info + "_" + str(json_response["gameId"]) + ".json", json_response)
                 add_complete_games(list_of_complete_games, json_response)
                 x += 1
     print("Every game loaded.")
@@ -195,16 +145,9 @@ def load_from_jsons(filename_account, filename_match_list, filename_match_info):
     show_me_everything(list_of_complete_games, champions)
 
 
-def main(load_from_files, filename_account, filename_match_list, filename_match_info):
-    if not load_from_files:
-        ask_the_riot_api(filename_account, filename_match_list, filename_match_info)
-    else:
-        load_from_jsons(filename_account, filename_match_list, filename_match_info)
-
-
 # Program starts here
 fileName_account_static = "download_json/account"
 fileName_match_list_static = "download_json/matchlist/matches"
 fileName_match_info_static = "download_json/matches/match_info"
-load_from_files_static = True
+load_from_files_static = False
 main(load_from_files_static, fileName_account_static, fileName_match_list_static, fileName_match_info_static)
